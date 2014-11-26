a91d44fcc203e2857e3682bc357abd5446c7d1f9
Agustin Krapovickas <agukrapo@gmail.com>
2014-11-26 12:23:24 -0600

    navigation fix

--- paginabanda/static/js/views/error.js
+++ paginabanda/static/js/views/error.js
@@ -1,7 +1,9 @@
 define([ 'jquery', 'bootstrap' ], function($, Bootstrap) {
   return {
     show: function(info) {
-      console.log('------> Error', info);
+      console.log('------> Error');
+      console.log(info.stack);
+      console.log(info);
       $('#error-msg-modal').modal();
     }    
   };
